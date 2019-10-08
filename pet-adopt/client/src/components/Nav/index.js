56beb39fb3fe66351f9b48c725ee4801d5119bac
John Falk <jrfalk@outlook.com>
2019-10-08 15:11:02 -0400

    Created initial files from template

---
+++ pet-adopt/client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <a className="navbar-brand" href="/">
+        Pet Search
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
