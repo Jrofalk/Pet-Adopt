be048a7a8bd63a8b74b60798454f12e185918a36
John Falk <jrfalk@outlook.com>
2019-10-14 15:14:49 -0400

    Created controller for User model

--- pet-adopt/models/user.js
+++ pet-adopt/models/user.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre('save', function(next) {
+    // Check if document is new or a new password has been set
+    if (this.isNew || this.isModified('password')) {
+      const document = this;
+      bcrypt.hash(document.password, saltRounds,
+        function(err, hashedPassword) {
+        if (err) {
+          next(err);
+        }
+        else {
+          document.password = hashedPassword;
+          next();
+        }
+      });
+    } else {
+      next();
+    }
+  });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
