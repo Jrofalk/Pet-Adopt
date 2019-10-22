e37b00cac0602fc9e8b145159813fd0f8c175a43
John Falk <jrfalk@outlook.com>
2019-10-22 13:20:22 -0400

    Getting mlab to work

--- pet-adopt/server.js
+++ pet-adopt/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/heroku_hkhvq2mb");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/adoptablepets");
 
 // adoptablepets
 
