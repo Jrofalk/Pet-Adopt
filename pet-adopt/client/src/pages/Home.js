b8725dc2ccce9e4ac7386decdc6978b15ce0bc8d
John Falk <jrfalk@outlook.com>
2019-10-22 15:53:57 -0400

    Fixed navbar on homepage by putting carousel in container

--- pet-adopt/client/src/pages/Home.js
+++ pet-adopt/client/src/pages/Home.js
@@ -27,52 +27,54 @@ class Home extends Component {
 
   render() {
     return (
-      <MDBCarousel
-        activeItem={1}
-        length={2}
-        showControls={true}
-        showIndicators={true}
-        className="z-depth-1"
-      >
-        <MDBCarouselInner style={{ height: "550px" }}>
-          <MDBCarouselItem itemId="1">
-            <MDBView>
-              <img 
-                className="d-block w-100"
-                src="https://www.tiffanybolkphotography.com/wp-content/uploads/2018/08/Cat-Photography-10.jpg"
-                // src={require("../assets/images/fritz.jpg")}
-                alt="First slide"
-              />
-              <MDBMask overlay="black-light" />
-            </MDBView>
-            <MDBCarouselCaption>
-              <h1 className="display-1" style={{ position: 'relative', fontFamily: 'Yantramanav, sans-serif', opacity: "0.7", bottom: "200px" }}>PET ADOPT</h1>
-              {/* <p>First text</p> */}
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId="2">
-            <MDBView>
-              <img
-                className="d-block w-100"
-                src="https://yuppypuppyspa.com/wp-content/uploads/2016/07/iStock_92168649_LARGE_1.jpg"
-                alt="Second slide"
-              />
-              <MDBMask overlay="black-strong" />
-            </MDBView>
-            <MDBCarouselCaption>
-              <h3 className="h3-responsive" style={{ position: 'relative', bottom: "0px", opacity: "0.7" }}>Find Your Best Friend</h3>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-        </MDBCarouselInner>
+      <MDBContainer>
+        <MDBCarousel
+          activeItem={1}
+          length={2}
+          showControls={true}
+          showIndicators={true}
+          className="z-depth-1"
+        >
+          <MDBCarouselInner style={{ height: "550px" }}>
+            <MDBCarouselItem itemId="1">
+              <MDBView>
+                <img
+                  className="d-block w-100"
+                  src="https://www.tiffanybolkphotography.com/wp-content/uploads/2018/08/Cat-Photography-10.jpg"
+                  // src={require("../assets/images/fritz.jpg")}
+                  alt="First slide"
+                />
+                <MDBMask overlay="black-light" />
+              </MDBView>
+              <MDBCarouselCaption>
+                <h1 className="display-1" style={{ position: 'relative', fontFamily: 'Yantramanav, sans-serif', opacity: "0.7", bottom: "200px" }}>PET ADOPT</h1>
+                {/* <p>First text</p> */}
+              </MDBCarouselCaption>
+            </MDBCarouselItem>
+            <MDBCarouselItem itemId="2">
+              <MDBView>
+                <img
+                  className="d-block w-100"
+                  src="https://yuppypuppyspa.com/wp-content/uploads/2016/07/iStock_92168649_LARGE_1.jpg"
+                  alt="Second slide"
+                />
+                <MDBMask overlay="black-strong" />
+              </MDBView>
+              <MDBCarouselCaption>
+                <h3 className="h3-responsive" style={{ position: 'relative', bottom: "0px", opacity: "0.7" }}>Find Your Best Friend</h3>
+              </MDBCarouselCaption>
+            </MDBCarouselItem>
+          </MDBCarouselInner>
+        </MDBCarousel>
         <MDBFooter color="white" className="font-small pt-4 mt-4">
           <MDBContainer fluid className="text-center text-md-left">
 
             <MDBContainer fluid>
-            <a className="nav-link text-center" href="https://github.com/Jrofalk/Pet-Adopt" target="_blank"><FontAwesomeIcon icon={faGithubAlt} style={{ color: 'black' , fontSize: "24px"}}/></a>
+              <a className="nav-link text-center" href="https://github.com/Jrofalk/Pet-Adopt" target="_blank"><FontAwesomeIcon icon={faGithubAlt} style={{ color: 'black', fontSize: "24px" }} /></a>
             </MDBContainer>
           </MDBContainer>
         </MDBFooter>
-      </MDBCarousel>
+      </MDBContainer>
     );
   }
 };
